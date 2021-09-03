37d37dc321dcc0375ac615e1794df8c15c258727
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-09-03 17:11:13 -0300

    Setup Navigation and Bottom Navigation

--- app/src/main/java/br/com/macmagazine/MainActivity.kt
+++ app/src/main/java/br/com/macmagazine/MainActivity.kt
@@ -2,10 +2,25 @@ package br.com.macmagazine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import br.com.macmagazine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navHostFragment.navController)
     }
 }
