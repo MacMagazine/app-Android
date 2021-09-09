14c4b77ec416ce8b5311f2153b342acc33553281
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-09-09 11:43:30 -0300

    Setup app koin module with data module dependencies

--- app/src/main/java/br/com/macmagazine/di/appModules.kt
+++ app/src/main/java/br/com/macmagazine/di/appModules.kt
@@ -1,8 +1,22 @@
 package br.com.macmagazine.di
 
+import br.com.macmagazine.data.repository.NewsRepositoryImpl
+import br.com.macmagazine.data.scrap.RssScraper
+import br.com.macmagazine.data.scrap.Scraper
+import br.com.macmagazine.domain.repository.NewsRepository
 import br.com.macmagazine.ui.main.post.list.PostListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+
+private val dataModule = module {
+    single<NewsRepository> {
+        NewsRepositoryImpl(get())
+    }
+
+    single<Scraper> {
+        RssScraper(get())
+    }
+}
 
 private val viewModelModule = module {
     viewModel {
@@ -10,6 +24,9 @@ private val viewModelModule = module {
     }
 }
 
-val appModules = listOf(viewModelModule)
+val appModules = listOf(
+    dataModule,
+    viewModelModule
+)
 
 
