eeb5266bb0e33701867bead1d85784b4580b5e81
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-09-23 19:44:50 -0300

    Change setup gradle of domain module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,13 +1,39 @@
+import extensions.implementation
 import dependencies.domainModuleDependencies
 
 plugins {
-    id("java-library")
-    id("kotlin")
+    id("com.android.library")
+    kotlin("android")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+android {
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk =  AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner  = AppConfig.androidTestInstrumentation
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
