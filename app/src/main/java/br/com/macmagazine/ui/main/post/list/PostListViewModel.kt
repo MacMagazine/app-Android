a5482ae31bc755f028f7953444473d4966bc9e5e
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2021-09-28 20:40:44 -0300

    Adjustments on post separators

--- app/src/main/java/br/com/macmagazine/ui/main/post/list/PostListViewModel.kt
+++ app/src/main/java/br/com/macmagazine/ui/main/post/list/PostListViewModel.kt
@@ -6,12 +6,15 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.insertSeparators
 import androidx.paging.map
+import br.com.macmagazine.common.isToday
+import br.com.macmagazine.common.isYesterday
 import br.com.macmagazine.common.toFormattedDate
 import br.com.macmagazine.mapper.toPostItemUi
 import br.com.macmagazine.model.PostUi
 import br.com.macmagazine.paging.PostPagingDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.time.LocalDate
 
 class PostListViewModel(
     private val postsDataSource: PostPagingDataSource
@@ -24,21 +27,31 @@ class PostListViewModel(
                 it.insertSeparators<PostUi.PostItemUi, PostUi>{ postBefore, postAfter ->
                     if (postAfter == null) {
                         // we're at the end of the list
-                        return@insertSeparators PostUi.PostHeaderDateUi("End of list")
+                        return@insertSeparators null
                     }
 
                     if (postBefore == null) {
                         // we're at the beginning of the list
-                        return@insertSeparators null
+                        createDateSeparator(postAfter.pubDate)
+                        return@insertSeparators createDateSeparator(postAfter.pubDate)
                     }
 
-                    if (postBefore.pubDate.isAfter(postAfter.pubDate)) {
-                        PostUi.PostHeaderDateUi(postBefore.pubDate.toFormattedDate().uppercase())
+                    if (postAfter.pubDate.isAfter(postBefore.pubDate)) {
+                        createDateSeparator(postAfter.pubDate)
                     } else {
                         null
                     }
                 }
             }
             .cachedIn(viewModelScope)
+    }
+
+    private fun createDateSeparator(date: LocalDate): PostUi.PostHeaderDateUi {
+        val label = when {
+            date.isToday() -> "HOJE"
+            date.isYesterday() -> "ONTEM"
+            else -> date.toFormattedDate().uppercase()
+        }
+        return PostUi.PostHeaderDateUi(label)
     }
 }
